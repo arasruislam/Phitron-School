ff02ede79ddb59f391daa9505fcbdc3e0cf46055
arasruislam <arasru01@gmail.com>
2024-06-28 11:44:18 +0600

    Login Logout in Django

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def get_course(req):
+    return render(req, "./base.html")
+
+
+def profile(req):
+    return render(req, "./profile.html")
